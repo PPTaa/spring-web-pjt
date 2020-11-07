d595b56c69711b47ffac32c968ddb06672c01256
Lee Jungchul <chobo05@naver.com>
2020-11-07 21:31:48 +0900

    business layer update

---
+++ pptaa_spring_pjt02/src/main/java/com/pptaa/service/BoardService.java
@@ -0,0 +1,15 @@
+package com.pptaa.service;
+
+import java.util.List;
+
+import com.pptaa.domain.BoardVO;
+
+public interface BoardService {
+
+	public void register(BoardVO board);
+	public BoardVO get(int bno);
+	public boolean modify(BoardVO board);
+	public boolean remove(int bno);
+	public List<BoardVO> getList();
+	
+}
---
+++ pptaa_spring_pjt02/src/test/java/com/pptaa/service/BoardServiceTests.java
@@ -0,0 +1,70 @@
+package com.pptaa.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.pptaa.domain.BoardVO;
+import com.pptaa.mapper.BoardMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardServiceTests {
+
+//	@Setter(onMethod_ = {@Autowired})
+	@Autowired
+	private BoardService service;
+	
+	@Test
+	public void testExist() {
+		log.info("+++++++++++++++++++++"+service);
+		assertNotNull(service);
+	}
+	
+	@Test
+	public void testRegister() {
+		BoardVO board = new BoardVO();
+		
+		board.setContent("testRegister");
+		board.setTitle("testRegister");
+		board.setWriter("testRegister");
+		
+		service.register(board);
+		
+		log.info("생성된 게시물의 번호 " + board.getBno());
+	}
+	
+	@Test
+	public void testGetList() {
+		service.getList().forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void testGet() {
+		log.info(service.get(1));
+	}
+	
+	@Test
+	public void testModify() {
+		BoardVO board = service.get(3);
+		if (board == null) {
+			return;
+		}
+		board.setTitle("제목 수정했음");
+		log.info("modify result : "+service.modify(board));
+	}
+	
+	@Test
+	public void testRemove() {
+		log.info("remove result : "+service.remove(5));
+	}
+	
+}
